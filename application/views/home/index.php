2daaab8b1b2edf58537013264ec504b12608a077
big-web-19 <bansos.dnr@gmail.com>
2022-05-09 14:14:17 +0700

    changing banner url

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -4,7 +4,7 @@
     <div class="col">
       <header class="header" style="
     color: #111;
-    background-image: url(https://res.cloudinary.com/dxlpys5st/image/upload/v1648126982/assets/TBC_FACEBOOK_COVER_qlhn1x.jpg);
+    background-image: url(https://res.cloudinary.com/dxlpys5st/image/upload/v1650827711/assets/TBC_FACEBOOK_COVER_rkbxny.jpg);
     padding: 10rem;
     background-position: center;
     background-size: cover;
